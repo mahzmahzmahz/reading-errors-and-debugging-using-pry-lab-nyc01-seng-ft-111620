538f1c4bc9a2e0ff3ab16297d5ada84517d836f9
Alex Marz <alexhmarz@gmail.com>
2020-10-31 16:49:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -4,11 +4,13 @@ def get_user_input
   gets.chomp
 end
 
+
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
+
 
 def selection(num)
   if num = 1
